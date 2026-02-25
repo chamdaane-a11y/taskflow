15fb781a048114145178d47b603711d385f3ba51
chamdaane-a11y <chamdaane@gmail.com>
2026-02-25 13:59:09 +0100

    fix database connexion Railway

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,12 @@
 import mysql.connector
+import os
 
 def connecter():
     connexion = mysql.connector.connect(
-        host="localhost",
-        user="hamdaane",
-        password="atanda",
-        database="todo_app"
+        host=os.getenv('MYSQLHOST', 'localhost'),
+        user=os.getenv('MYSQLUSER', 'root'),
+        password=os.getenv('MYSQLPASSWORD', ''),
+        database=os.getenv('MYSQLDATABASE', 'todo_app'),
+        port=int(os.getenv('MYSQLPORT', 3306))
     )
     return connexion
